dca10dff9fe2d90b4870b981c689e1cf8e4a5b9a
movie42 <movie3342@gmail.com>
2022-11-05 19:39:09 +0900

    svg, font-family setting

--- src/lib/style/GlobalStyles.tsx
+++ src/lib/style/GlobalStyles.tsx
@@ -52,7 +52,7 @@ table {
 }
 
 html{
-    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    font-family: 'Roboto', sans-serif;
     font-size:10px;
 }
 body{
